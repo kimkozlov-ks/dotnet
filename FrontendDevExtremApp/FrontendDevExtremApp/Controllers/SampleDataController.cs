4ce25fef8c757a6101ccf57667719a43f3c6ae3d
kimkozlov <kimkozlov.ks@gmail.com>
2020-03-09 15:38:36 +0200

    Fix component script, check getting data from local storage

--- FrontendDevExtremApp/FrontendDevExtremApp/Controllers/SampleDataController.cs
+++ FrontendDevExtremApp/FrontendDevExtremApp/Controllers/SampleDataController.cs
@@ -7,12 +7,14 @@ using DevExtreme.AspNet.Data;
 using DevExtreme.AspNet.Mvc;
 using Microsoft.AspNetCore.Mvc;
 using Services;
+using DataModel.Models;
 
 namespace FrontendDevExtremApp.Controllers {
 
     [Route("api/[controller]")]
     public class SampleDataController : Controller {
 
+  
         [HttpGet]
         public object Get(DataSourceLoadOptions loadOptions) 
         {
@@ -20,5 +22,14 @@ namespace FrontendDevExtremApp.Controllers {
 
             return DataSourceLoader.Load(dataLoader.GetData(), loadOptions);
         }
+        /*
+        [HttpGet]
+        public object TaskDetails(string phone, DataSourceLoadOptions loadOptions)
+        {
+            var dataLoader = new DataLoader();
+
+            List<User> masterDetails = dataLoader.GetData().Where(e => e.Phone == phone).ToList();
+            return DataSourceLoader.Load(masterDetails, loadOptions);
+        }*/
     }
 }
